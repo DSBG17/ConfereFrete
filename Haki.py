c4183631c253964db0fb765c718b105e87773630
Davi <davi.carlos.santos17@gmail.com>
2024-11-21 08:34:13 -0300

    Atualização

--- Haki.py
+++ Haki.py
@@ -6,5 +6,17 @@ app = Flask (__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/calculos')
+def calculo():
+    return render_template ("calculos.html")
+
+@app.route('/varredura')
+def varredura():
+    return render_template("varredura.html")
+
+@app.route('/extrato')
+def extrato():
+    return render_template("extrato.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
